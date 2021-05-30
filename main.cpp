09abf6eaa8c2c3ce3d9c7cd463525b85de5fb1ec
watchbutdonotlearn <71526591+watchbutdonotlearn@users.noreply.github.com>
2021-05-30 14:16:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,26 @@ std::string execute(const std::string& command) {
 
 int main(int argc, char *argv[])
 {
+	int usewhichnc = 999;
+	if(execute("whereis nc").length() > 3) {
+		usewhichnc = 0;
+	}else{
+		if(execute("where ncat").length() > 5) {
+			usewhichnc = 1;
+		}else{
+			if(execute("where netcat").length() > 7){
+				usewhichnc = 2;
+			}
+		}
+	}
+	if(usewhichnc == 999){
+		std::cout << "No netcat found. ";
+	}
+	std::cout << "debug: ";
+	std::cout << usewhichnc << " ";
+	std::string nclookup[3] = {"nc", "ncat", "netcat"};
+	std::string ncnum = nclookup[usewhichnc];
+	std::cout << ncnum << " ";
 	if( argc >= 2 ) {
 		std::cout << argc << "\n";
 		std::string arg1 = argv[1];
@@ -34,7 +54,9 @@ int main(int argc, char *argv[])
 			}
 			if(argc == 2){
 				std::cout << "Use command on sender computer: " << argv[0] << " send file.txt " << localip;
-				system("nc -l -p 1234 > out.file");
+				std::string rcmd = ncnum + " -l -p 1234 > out.file";
+				char* rcommand = const_cast<char*>(rcmd.c_str());
+				system(rcommand);
 			}
 		}
 		else if((arg1 == "send") || (arg1 == "Send")){
@@ -44,7 +66,7 @@ int main(int argc, char *argv[])
 				if(arg3.length() <= 3){
 					arg3 = "192.168.0." + arg3;
 				}
-				std::string cmd = "nc -w 3 " + arg3 + " 1234 < " + arg2;
+				std::string cmd = ncnum + " -w 3 " + arg3 + " 1234 < " + arg2;
 				char* command = const_cast<char*>(cmd.c_str());
 				std::cout << "Send command used with code " << arg2 << " " << arg3 << "\n";
 				std::cout << command << "\n";
